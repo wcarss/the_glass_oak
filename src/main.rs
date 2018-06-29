062483c6c5e7e2a696b519e768f9e34a632920b9
Wyatt Carss <carss.w@gmail.com>
2018-06-29 18:44:23 -0400

    Create and use an Object class.

--- src/main.rs
+++ src/main.rs
@@ -2,53 +2,92 @@ extern crate tcod;
 
 use tcod::console::*;
 use tcod::colors;
+use tcod::Color;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
+
+struct Object {
+  x: i32,
+  y: i32,
+  char: char,
+  color: Color,
+}
+
+impl Object {
+  pub fn new (x: i32, y: i32, char: char, color: Color) -> Self {
+    Object {
+      x: x,
+      y: y,
+      char: char,
+      color: color,
+    }
+  }
+
+  pub fn move_by (&mut self, dx: i32, dy: i32) {
+    self.x += dx;
+    self.y += dy;
+  }
+
+  pub fn draw (&self, con: &mut Console) {
+    con.set_default_foreground(self.color);
+    con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+  }
+
+  pub fn clear (&self, con: &mut Console) {
+    con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+  }
+}
 
 fn main() {
   let mut root = Root::initializer()
     .font("square10x10.png", FontLayout::Tcod)
     .font_type(FontType::Greyscale)
     .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-    .title("Rust/libtcod tutorial")
+    .title("The Glass Oak")
     .init();
 
   let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
   tcod::system::set_fps(LIMIT_FPS);
 
-  let mut player_x = SCREEN_WIDTH / 2;
-  let mut player_y = SCREEN_HEIGHT / 2;
+  let player = Object::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', colors::WHITE);
+  let npc = Object::new(SCREEN_WIDTH/2-5, SCREEN_HEIGHT/2, '@', colors::YELLOW);
+  let mut objects = [player, npc];
 
   while !root.window_closed() {
-    con.set_default_foreground(colors::WHITE);
-    con.put_char(player_x, player_y, '@', BackgroundFlag::None);
+    for object in &objects {
+      object.draw(&mut con);
+    }
+
     blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
 
     root.flush();
 
-    con.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+    for object in &objects {
+      object.clear(&mut con);
+    }
 
-    let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+    let player = &mut objects[0];
+    let exit = handle_keys(&mut root, player);
     if exit {
       break
     }
   }
 }
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
   use tcod::input::Key;
   use tcod::input::KeyCode::*;
 
   let key = root.wait_for_keypress(true);
 
   match key {
-    Key { code: Up, .. } => *player_y -= 1,
-    Key { code: Down, .. } => *player_y += 1,
-    Key { code: Left, .. } => *player_x -= 1,
-    Key { code: Right, .. } => *player_x += 1,
+    Key { code: Up, .. } => player.y -= 1,
+    Key { code: Down, .. } => player.y += 1,
+    Key { code: Left, .. } => player.x -= 1,
+    Key { code: Right, .. } => player.x += 1,
 
     Key { code: Enter, alt: true, .. } => {
       let fullscreen = root.is_fullscreen();
