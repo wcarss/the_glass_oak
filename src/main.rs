3b322878a25b423e7a486f7b4d7dce12230b2a29
Wyatt Carss <carss.w@gmail.com>
2018-06-30 13:52:08 -0400

    Reorganize code a littel for visual appeal.

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,49 @@ impl Object {
   }
 }
 
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
+  for object in objects {
+    object.draw(con);
+  }
+
+  for y in 0 .. MAP_HEIGHT {
+    for x in 0 .. MAP_WIDTH {
+      let wall = map[x as usize][y as usize].block_sight;
+      if wall {
+        con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
+      } else {
+        con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
+      }
+    }
+  }
+
+  blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
+  use tcod::input::Key;
+  use tcod::input::KeyCode::*;
+
+  let key = root.wait_for_keypress(true);
+
+  match key {
+    Key { code: Up, .. } => player.move_by(0, -1, map),
+    Key { code: Down, .. } => player.move_by(0, 1, map),
+    Key { code: Left, .. } => player.move_by(-1, 0, map),
+    Key { code: Right, .. } => player.move_by(1, 0, map),
+
+    Key { code: Enter, alt: true, .. } => {
+      let fullscreen = root.is_fullscreen();
+      root.set_fullscreen(!fullscreen);
+    },
+    Key { code: Escape, .. } => return true,
+
+    _ => {},
+  }
+
+  false
+}
+
 fn main() {
   let mut root = Root::initializer()
     .font("square10x10.png", FontLayout::Tcod)
@@ -111,47 +154,4 @@ fn main() {
       break
     }
   }
-}
-
-fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
-  use tcod::input::Key;
-  use tcod::input::KeyCode::*;
-
-  let key = root.wait_for_keypress(true);
-
-  match key {
-    Key { code: Up, .. } => player.move_by(0, -1, map),
-    Key { code: Down, .. } => player.move_by(0, 1, map),
-    Key { code: Left, .. } => player.move_by(-1, 0, map),
-    Key { code: Right, .. } => player.move_by(1, 0, map),
-
-    Key { code: Enter, alt: true, .. } => {
-      let fullscreen = root.is_fullscreen();
-      root.set_fullscreen(!fullscreen);
-    },
-    Key { code: Escape, .. } => return true,
-
-    _ => {},
-  }
-
-  false
-}
-
-fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
-  for object in objects {
-    object.draw(con);
-  }
-
-  for y in 0 .. MAP_HEIGHT {
-    for x in 0 .. MAP_WIDTH {
-      let wall = map[x as usize][y as usize].block_sight;
-      if wall {
-        con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-      } else {
-        con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-      }
-    }
-  }
-
-  blit(con, (0, 0), (MAP_WIDTH, MAP_HEIGHT), root, (0, 0), 1.0, 1.0);
 }
